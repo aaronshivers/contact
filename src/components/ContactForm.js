16a43aed6f116775cd6d2caff18d87bcbe4aabcb
aaronshivers <aaron@moonmail.org>
2019-07-25 10:44:46 -0500

    added custom validation messages

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,57 +1,93 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Form, Row, Col, Button } from 'react-bootstrap'
 
-const ContactForm = () => (
-  <Form>
-    <Row>
-      <Col sm={ 6 }>
-        {/* Name */}
-        <Form.Group controlId="name">
-          <Form.Label>Name</Form.Label>
-          <Form.Control type="text" placeholder="Enter name" />
-        </Form.Group>
-      </Col>
+const ContactForm = () => {
+  const [ validated, setValidated ] = useState(false)
 
-      <Col sm={ 6 }>
-        {/* Email */}
-        <Form.Group controlId="email">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
-        </Form.Group>
-      </Col>
-    </Row>
+  const handleSubmit = event => {
+    const form = event.currentTarget
 
-    <Row>
-      <Col>
-        {/* Subject */}
-        <Form.Group controlId="subject">
-          <Form.Label>Subject</Form.Label>
-          <Form.Control type="text" placeholder="Enter subject" />
-        </Form.Group>
-      </Col>
-    </Row>
+    if (form.checkValidity() === false) {
+      event.preventDefault()
+      event.stopPropagation()
+    }
+
+    setValidated(true)
+  }
+
+  return (
+    <Form noValidate validated={ validated } onSubmit={ handleSubmit }>
+      <Row>
+        <Col sm={ 6 }>
+          {/* Name */}
+          <Form.Group controlId="name">
+            <Form.Control
+              size="lg"
+              type="text"
+              placeholder="Name..."
+              required
+            />
+          <Form.Control.Feedback type="invalid">Please enter your name.</Form.Control.Feedback>
+          </Form.Group>
+        </Col>
+
+        <Col sm={ 6 }>
+          {/* Email */}
+          <Form.Group controlId="email">
+            <Form.Control
+              size="lg"
+              type="email"
+              placeholder="Email..."
+              required
+            />
+          <Form.Control.Feedback type="invalid">Please enter your email.</Form.Control.Feedback>
+          </Form.Group>
+        </Col>
+      </Row>
+
+      <Row>
+        <Col>
+          {/* Subject */}
+          <Form.Group controlId="subject">
+            <Form.Control
+              size="lg"
+              type="text"
+              placeholder="Subject..."
+              required
+            />
+          <Form.Control.Feedback type="invalid">Please enter the subject.</Form.Control.Feedback>
+          </Form.Group>
+        </Col>
+      </Row>
 
 
-    <Row>
-      <Col>
-        {/* Message */}
-        <Form.Group controlId="message">
-          <Form.Label>message</Form.Label>
-          <Form.Control as="textarea" rows="3" placeholder="Enter message" />
-        </Form.Group>
-      </Col>
-    </Row>
+      <Row>
+        <Col>
+          {/* Message */}
+          <Form.Group controlId="message">
+            <Form.Control
+              size="lg"
+              as="textarea"
+              rows="3"
+              placeholder="Message..."
+              required
+            />
+          <Form.Control.Feedback type="invalid">Please enter a message.</Form.Control.Feedback>
+          </Form.Group>
+        </Col>
+      </Row>
 
-    <Row>
-      <Col xs={ 3 }>
-        {/* Button */}
-        <Button variant="primary" type="submit">
-          Submit
-        </Button>
-      </Col>
-    </Row>
+      <Row>
+        <Col xs={ 3 }>
+          {/* Button */}
+          <Button size="lg" variant="primary" type="submit">
+            Submit
+          </Button>
+        </Col>
+      </Row>
 
-  </Form>
-)
+    </Form>
+  )
+}
 
 export default ContactForm
